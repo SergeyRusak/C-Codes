97dc4c0d20da078ee204f92d3845a3ecab551a4b
SergeyRusak <rusak200281@gmail.com>
2020-11-26 22:17:52 +0800

    egg complete
    Переделана функция search2

--- eggs.cpp
+++ eggs.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <locale.h>
+#include <math.h>
 
 
 
@@ -14,13 +15,9 @@ int powof(int a, int N){
     return otv;
 }
 int search2 (int N)
-{
-    int result=100000;
-    for (int i =1; i<=N;i++)
-    {
-        if (result > (N+i-1)/i+i-1) result = (N+i-1)/i+i-1;
-    }
-    return result;
+{ 
+   
+    return ceil(((sqrt(1+8*N)-1)/2.0));
 }
 
 int powin( int a, int b){
@@ -29,41 +26,4 @@ int powin( int a, int b){
   otv*=a;   
  }
  return otv;
-}
-
-
-
-int main()
-{ 
-setLocale(LC_ALL,"Russian");   
-    
-int H, nE,E, otv;
-float temp;
-
-
-do
-{
-    std::cout <<"Введите высоту здания :";
-    std::cin >>temp;
-}
-while(temp<0 || temp-(int)temp!=0);
-H=(int)temp;
-
-
-do
-{
-    std::cout <<"Введите количество яиц :";
-    std::cin >>temp;
-}
-while(temp<0 || temp-(int)temp!=0);
-E=(int)temp;
-
-
-nE = powof(2,H);
-if (E==1) otv = H;
-    else if (E==2) otv = search2(H);
-        else if (E>2 && E<nE) otv =search2 (H/powin(2,E-2))+E-2;
-            else otv = nE;
-    
- std::cout<<""<<otv<<std::endl;
 }
