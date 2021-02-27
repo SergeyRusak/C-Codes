1ba6219406486a4fc2f6c2a29f108332820f6ffa
Sergey Rusak <51901329+SergeyRusak@users.noreply.github.com>
2021-02-27 09:56:20 +0800

    Update fibonachi.cpp

--- fibonachi.cpp
+++ fibonachi.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
-#include "ConsoleApplication3.h"
+#include <locale.h>
 
 
-
-
-long long* Fib(int n) {
+long long* Fib(int n, int *last) {
     long long* a = new long long[n];
     a[0] = a[1] = 1;
-    int number=1;
     for (int i = 2; i < n; i++)
     {
         if (a[i - 1] + a[i - 2] < INT_MAX) {
             a[i] = a[i - 1] + a[i - 2];
-            number++;
+      
         }
-        else 
+        else
         {
-            std::cout << "last number: " << number <<std::endl;
+            *last = i;
             break;
         }
+        *last = -1;
 
     }
-    
+
 
     return a;
 
@@ -32,19 +30,24 @@ long long* Fib(int n) {
 int main()
 {
 
-    const int FIB_COUNT = 115;
+    setlocale(LC_ALL, "Russian");
+    int FIB_COUNT;
+    std::cout << "Введите количество чисел :";
+    std::cin >> FIB_COUNT;
+
+    
     int last_num = 0;
-    long long* fib_arr = Fib(FIB_COUNT);
+    long long* fib_arr = Fib(FIB_COUNT, &last_num);
 
- 
-        for (int i = 0; i < FIB_COUNT; i++)
-        {
-            if (fib_arr[i] < 0) {
-                std::cout << " Too much numbers!!!";
-                break;
+
+    for (int i = 0; i < FIB_COUNT; i++)
+    {
+        if (last_num > -1) {
+            std::cout << "Числа не помещаются в выделенную память. Последний номер числа:"<<last_num;
+
+            break;
         }
-            std::cout << fib_arr[i] << " ";
-        }
-  
+        std::cout << fib_arr[i] << " ";
+    }
+
 }
-
