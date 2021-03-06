c889aae1b704f99bc0900f9ff95999eec863a0b5
Sergey Rusak <51901329+SergeyRusak@users.noreply.github.com>
2021-03-06 10:27:21 +0800

    Update text_formate.cpp

--- text_formate.cpp
+++ text_formate.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <locale.h>
-#include <climits>
 #include <string>
+#include <windows.h>
 
 std::string formate(std::string line, int format, int max_lenght) {
     int free_space = max_lenght - line.length();
@@ -10,7 +10,7 @@ std::string formate(std::string line, int format, int max_lenght) {
     switch (format)
     {
     case 0:  //Formating.left
-        for  ( int i = 0; i < free_space; i++)
+        for (int i = 0; i < free_space; i++)
         {
             line.append(" ");
 
@@ -21,7 +21,7 @@ std::string formate(std::string line, int format, int max_lenght) {
         break;
     case 1:  //Formating.center
         for (int i = 0; i < free_space; i++)
-        { 
+        {
             if (i % 2 == 0) line.insert(0, " ");
             else line.append(" ");
 
@@ -33,7 +33,7 @@ std::string formate(std::string line, int format, int max_lenght) {
     case 2:  //Formating.right
         for (int i = 0; i < free_space; i++)
         {
-            line.insert(0," ");
+            line.insert(0, " ");
 
         }
         break;
@@ -43,26 +43,36 @@ std::string formate(std::string line, int format, int max_lenght) {
 
         for (int i = 0; i < line.length(); i++) {
             if (line[i] == ' ') space_count++;
-        }
-
-        int space_width = free_space / space_count;
+        }   
+        int space_width =(space_count!=0)?( free_space / space_count):(0);
         std::string space = "";
-
+        
         for (int i = 0; i < space_width; i++) {
             space.append(" ");
         }
-
+        
         for (int i = 0; i < line.length(); i++) {
+            
             if (line[i] == ' ') {
                 line.insert(i, space);
-                i += space_count;
-            }
+                i += space_width;
+                
+            }          
         }
 
-        for (int i = 0; i < free_space - space_count * space_width; i++)
-        {
-            line.append(" ");
+        int last_space = free_space - (space_count * space_width);
+        for (int i = 0; i < line.length()-1; i++) {
+            if (line[i] == ' ' && line[i + 1] != ' ') 
+            {
+                line.insert(i, " ");
+                i++;
+                last_space--;
+                if (last_space == 0) break;
+            }
+
         }
+
+
         break;
     }
 
@@ -71,39 +81,42 @@ std::string formate(std::string line, int format, int max_lenght) {
         break;
     }
     return line;
-        
+
 
 }
 
 std::string clearSpace(std::string line) {
-   
-    for (int i = line.length()-1; i>=0; i--)
+    for (int i = line.length() - 1; i >= 0; i--)
     {
         if (line[i] == ' ') {
-            if (i == 0 || i == line.length()-1) line.erase(i);
-            else if(line[i-1] == ' ') line.erase(i);
-            else if(line[i + 1] == '.' || line[i + 1] == ','|| line[i + 1] == '!' || line[i + 1] == '?') line.erase(i);    
+            if (i == 0 || i == line.length() - 1) line.erase(i,1);
+            else if (line[i - 1] == ' ') line.erase(i,1);
+            else if (line[i + 1] == '.' || line[i + 1] == ',' || line[i + 1] == '!' || line[i + 1] == '?') line.erase(i);
         }
-       
+
     }
     return line;
 }
 
 int main()
 {
-
-    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     int text_COUNT;
     unsigned int MAX_LENGHT = 80;
-   
+
     std::cout << "Введите количество строк в тексте:";
     std::cin >> text_COUNT;
-    std::string *text = new std::string [text_COUNT];
+    std::string* text = new std::string[text_COUNT];
 
     std::cout << "Введите текст" << std::endl;
-    std::cout << "ВНИМАНИЕ! Все символы, не поместившиеся в диапозон " << MAX_LENGHT << "-и символов удаляться!" << std::endl;
+    std::cout << "ВНИМАНИЕ! Все символы, не поместившиеся в диапозон " << MAX_LENGHT << "-и символов будут удаляться!" << std::endl;
     for (int i = 0; i < text_COUNT; i++) {
+
         std::getline(std::cin, text[i]);
+
+        while (text[i].length() ==0) std::getline(std::cin, text[i]);
+
         if (text[i].length() > MAX_LENGHT) {
             text[i].erase(MAX_LENGHT, text[i].length() - MAX_LENGHT);
         }
@@ -120,7 +133,7 @@ int main()
     for (int i = 0; i < text_COUNT; i++)
     {
         text[i] = formate(clearSpace(text[i]), tf, MAX_LENGHT);
-        
+
     }
 
     for (int i = 0; i < text_COUNT; i++)
@@ -131,6 +144,6 @@ int main()
     }
 
 
-   
+
 
 }
