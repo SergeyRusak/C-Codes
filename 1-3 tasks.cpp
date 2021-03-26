d58f303e132a238a46d2bf83dfbead2cc9e2b805
Sergey Rusak <51901329+SergeyRusak@users.noreply.github.com>
2021-03-26 18:20:49 +0800

    Update 1-3 tasks.cpp

--- 1-3 tasks.cpp
+++ 1-3 tasks.cpp
@@ -1,38 +1,33 @@
 #include <iostream>
 #include <string>
 
-double power(double n, double origin, int a) {
-    return (a > 1) ? power(n * origin, origin, a - 1) : n;
-}
 
 double power(double n, int a) {
-    return (a > 1) ? power(n * n, n, a - 1) : n ;
+    return (a > 0) ? (n*power(n, a - 1)) : (1);
 }
 
 
-int sum(int n, int summ) {
-    return (n / 10 > 0) ? (sum(n / 10, summ + n % 10)) : (summ + n);
 
-}
 int sum(int origin) {
-    return (origin / 10 > 0) ? (sum(origin / 10, origin % 10)) : (origin);
+    return (origin / 10 > 0) ? (origin % 10 + sum(origin / 10)) : (origin);
 }
 
 
 
-
-std::string rev(std::string v) {
-    std::reverse(v.begin(), v.end());
-    return v;
-}
-
-std::string n_to_base(int N, int base, std::string code) {
-    return (N / base != 0) ? (n_to_base(N/base, base, code +std::to_string(N % base))) : (rev(code+ std::to_string(N) ));
-
-}
-
-std::string n_to_base(int N, int base) {
-   return (N / base != 0) ? (n_to_base(N / base, base,std::to_string(N % base))) : (std::to_string(N));
+void n_to_base(int n , int base) {
+    if (n < 0) {
+        std::cout << "-";
+        n_to_base(-n, base);
+    }
+    else if (n < base) {
+        std::cout << n;
+        return;
+    }
+    else {
+        n_to_base(n / base, base);
+        int res = n % base;
+        std::cout << res;
+    }
 }
 
 
@@ -43,7 +38,7 @@ std::string n_to_base(int N, int base) {
 
 int main()
 {
-    std::cout <<"2 pow 5 ="<< power(2, 5) <<"  (32)"<< std::endl;
-    std::cout <<"sum 123456 = "<< sum(123456) <<"  (21)"<< std::endl;
-    std::cout << "64 in base 2 = " << n_to_base(64,2) <<"  (1000000)"<< std::endl;
+    std::cout << "2 pow 5 =" << power(2, 5) << "  (32)" << std::endl;
+    std::cout << "sum 123456 = " << sum(123456) << "  (21)" << std::endl;
+    std::cout << "64 in base 2 = "; n_to_base(64, 2);std::cout << "  (1000000)" << std::endl;
 }
