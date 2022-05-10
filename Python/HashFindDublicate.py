7e56a72e9951fbedf01b03f33fc6a4d4e7da4fc2
Sergey Rusak <51901329+SergeyRusak@users.noreply.github.com>
2022-05-10 17:54:28 +0800

    Create HashFindDublicate.py

---
+++ Python/HashFindDublicate.py
@@ -0,0 +1,81 @@
+import re
+import os
+import time
+
+def CRC(chars):
+    h = 0
+    for char in chars:
+        highorder = h & 0xf8000000
+        h = h << 5
+        h = h ^ (highorder >> 27)
+        h = h ^ ord(char)
+    return h
+
+
+def PJW(chars):
+    h = 0
+    for char in chars:
+        h = (h << 4) + ord(char)
+        g = h & 0xf0000000
+        if g != 0:
+            h = h ^ (g >> 24)
+            h = h ^ g
+    return h
+
+
+def BUZ(chars):
+    h = 0
+    for char in chars:
+        highorder = h & 0x80000000
+        h = h << 1
+        h = h ^ (highorder >> 31)
+        h = h ^ 0  # TODO: RAND
+
+
+def prepare(text):
+    return re.sub('\W+', '', text.lower())
+
+
+def check_dublicate(files_list, hash_func):
+    hashes = []
+    count = 0
+    for file in files_list:
+        with open(file, "r") as f:
+            tex = prepare(f.read())
+            hashes.append(hash_func(tex))
+    hashes.sort()
+    for i in range(len(hashes) - 1):
+        if hashes[i] == hashes[i + 1] == 0:
+            count += 1
+    return count
+
+def find_files(path):
+    filelist = []
+    for (root, dirs, file) in os.walk(path):
+        for f in file:
+            filelist.append(os.path.join(root, f))
+    return filelist
+
+
+def timetest(files, func):
+    print("____________________________")
+    print("Hash-func : "+ (func.__name__ if func.__name__ !="hash" else "build-in hash function"))
+    timer = time.time_ns()
+    print("Dublicate files : " + str(check_dublicate(files, func)))
+    print("Elapsed time: " + str((time.time_ns() - timer) / 1000) + " milliseconds")
+    print("____________________________")
+
+
+
+if __name__ == "__main__":
+
+    hash1 = CRC
+    hash2 = PJW
+    hash3 = hash
+
+    files = find_files(".")
+
+    timetest(files, hash1)
+    timetest(files, hash2)
+    timetest(files, hash3)
+
