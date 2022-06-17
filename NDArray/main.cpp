194c14106ad06cea20416673b655a4dba1163ad4
Sergey Rusak <51901329+SergeyRusak@users.noreply.github.com>
2022-06-17 09:42:08 +0800

    Update main.cpp

--- NDArray/main.cpp
+++ NDArray/main.cpp
@@ -4,28 +4,30 @@
 int main()
 {
 
-    std::vector<int> testsize;
+     std::vector<int> testsize;
     testsize.push_back(2);
-    testsize.push_back(2);
+    testsize.push_back(3);
 
     NDArray<int> test(testsize);
     test.set(0, 1);
     test.set(1, 2);
     test.set(2, 3);
     test.set(3, 4);
-
+    test.set(4, 5);
+    test.set(5, 6);
+    NDArray<int> transptest = test.MatrixTransp();
     std::cout << test.get(1)<<std::endl;
     std::cout << test.MatrixTransp().get(1) << std::endl;
     
     
-    NDArray<int> test2(3, 2, 2, 2);
-    test2.set(0, 2);
-    NDArray<int>test21(NDArray<int>::one(2,2,2));
-    NDArray<int>test22 = NDArray<int>::rand(2,2,2);
-    std::cout << test22.get(0) << std::endl;
-    std::cout << (test22-test21).get(0) << std::endl;
-    NDArray<int>test3 = test22.MatrixMultiply(test21);
-    std::cout << test3[0];
+    for (int i = 0; i < 6; i++) {
+        std::cout << test.get(i)<< ' ';
+    }
+    std::cout<< std::endl;
+    for (int i = 0; i < 6; i++) {
+        std::cout << transptest.get(i)<< ' ';
+    }
+    
 
 }
 
